2740a559a7afe00295c198ac7aa15045ba119561
rexileer <110720074+rexileer@users.noreply.github.com>
2025-03-27 18:23:09 +0300

    Update remove_user_service.py
    fix

--- bot/services/remove_user_service.py
+++ bot/services/remove_user_service.py
@@ -20,16 +20,13 @@ async def remove_user_from_db(user_id: int):
     Удаляет пользователя и связанные с ним фильтры из базы данных.
     """
     try:
-        async with transaction.atomic():
-            # Удаляем фильтры пользователя
-            await UserFilters.objects.filter(user_id=user_id).adelete()
+        await UserFilters.objects.filter(user_id=user_id).adelete()
+        # Удаляем самого пользователя
+        deleted, _ = await User.objects.filter(user_id=user_id).adelete()
             
-            # Удаляем самого пользователя
-            deleted, _ = await User.objects.filter(id=user_id).adelete()
-            
-            if deleted:
-                logger.info(f"Пользователь {user_id} успешно удален")
-            else:
-                logger.warning(f"Пользователь {user_id} не найден")
+        if deleted:
+            logger.info(f"Пользователь {user_id} успешно удален")
+        else:
+            logger.warning(f"Пользователь {user_id} не найден")
     except Exception as e:
         logger.error(f"Ошибка при удалении пользователя {user_id}: {e}")
